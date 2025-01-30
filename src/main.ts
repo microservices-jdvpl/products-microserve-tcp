8b3fb8a12fab83f6fdb57a5cee1a892b7b582d53
jdsuarez <jdsuarez@fgs.co>
2025-01-29 22:37:58 -0500

    remove unnecessary console log from main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('main');
-  console.log('hola mundo');
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
